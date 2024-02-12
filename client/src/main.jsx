1287e9e8c822ac4e6800dcf68f961f53291e7111
RaulSierradev <illosierra@outlook.com>
2024-02-12 14:36:48 -0500

    Migracion de create-react-app a vite

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,12 +6,10 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
-    <App />
-  </BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
-  ,
-  document.getElementById("root")
 );
